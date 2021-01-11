f987cf4839697793aa112d275f17779d19b08af7
Dael Muñiz <daelvn@gmail.com>
2021-01-11 22:57:40 +0100

    Removed hidden dependency

--- tableview.lua
+++ tableview.lua
@@ -1,11 +1,10 @@
 local mxml = require("moonxml")
 local loadkit = require("loadkit")
-local inspect = require("inspect")
 local moonxmlFind = loadkit.make_loader("moonxml")
 local generate
 generate = function(tbl)
   local template = mxml.html:loadmoonfile(moonxmlFind("tableview.template"))
-  return template(tbl, inspect)
+  return template(tbl)
 end
 return {
   generate = generate
--- tableview.moon
+++ tableview.moon
@@ -1,11 +1,10 @@
 mxml    = require "moonxml"
 loadkit = require "loadkit" 
-inspect = require "inspect"
 
 moonxmlFind = loadkit.make_loader "moonxml"
 
 generate = (tbl) ->
   template = mxml.html\loadmoonfile moonxmlFind "tableview.template"
-  return template tbl, inspect
+  return template tbl
 
 { :generate }
